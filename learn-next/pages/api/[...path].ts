85294fe1b81cc854d40653fd69e8d2959f9e527c
ducvinh-dev <ducvinh.dev@gmail.com>
2023-03-26 14:21:12 +0700

    feat: add http-proxy

---
+++ learn-next/pages/api/[...path].ts
@@ -0,0 +1,26 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import httpProxy from "http-proxy";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const proxy = httpProxy.createProxyServer();
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  // don't send cookies to api server
+  req.headers.cookie = "";
+  proxy.web(req, res, {
+    target: process.env.API_URL,
+    changeOrigin: true,
+    selfHandleResponse: false,
+  });
+
+  // res.status(200).json({ name: "PATH - MATCH ALL HERE" });
+}
